b224d357bdc4bbf933fccd79e28970eaa8d739d8
soup <shawnazy02@gmail.com>
2022-11-22 06:13:11 -0800

    2

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -11,7 +11,7 @@ public class PlayerShoot : MonoBehaviour
     
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
